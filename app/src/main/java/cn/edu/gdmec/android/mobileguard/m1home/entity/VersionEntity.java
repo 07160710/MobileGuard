956e515e9b0369c9360ec757fb4c45c0784e6083
huangkaifeng <1195745286@qq.com>
2017-09-13 17:12:12 +0800

    全部提交

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+
+public class VersionEntity {
+    public String versionCode;
+    public String description;
+    public String apkurl;
+}
