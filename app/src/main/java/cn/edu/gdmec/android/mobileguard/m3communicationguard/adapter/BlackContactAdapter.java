558ce42eaa69c20d8063d951c750f15ee6c63d66
huangkaifeng <1195745286@qq.com>
2017-10-30 12:29:37 +0800

    建立结构文件10-30

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
@@ -0,0 +1,31 @@
+package cn.edu.gdmec.android.mobileguard.m3communicationguard.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Administrator on 2017/10/30 0030.
+ */
+
+public class BlackContactAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
