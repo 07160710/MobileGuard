693621c2d7d954d89e4fe3a1abf43a08ba43fd9c
huangkaifeng <1195745286@qq.com>
2017-11-20 17:22:54 +0800

    m6缓存完成结构搭建及部分代码实现

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/CacheClearListActivity.java
@@ -0,0 +1,21 @@
+package cn.edu.gdmec.android.mobileguard.m6cleancache;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+public class CacheClearListActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cache_clear_list);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
