6b22541e211893dd75c9df65502dd43e2df2c6aa
huangkaifeng <1195745286@qq.com>
2017-11-28 21:39:22 +0800

    m5修改部分代码

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/utils/UrlClient.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/utils/UrlClient.java
@@ -51,13 +51,13 @@ public class UrlClient {
             int respondCode = mHttpURLConnection.getResponseCode();
             //Log.d("respondCode","respondCode="+respondCode );
             // 获取返回内容类型
-            String type = mHttpURLConnection.getContentType();
+            //String type = mHttpURLConnection.getContentType();
             //Log.d("type", "type="+type);
             // 获取返回内容的字符编码
-            String encoding = mHttpURLConnection.getContentEncoding();
+            //String encoding = mHttpURLConnection.getContentEncoding();
             //Log.d("encoding", "encoding="+encoding);
             // 获取返回内容长度，单位字节
-            int length = mHttpURLConnection.getContentLength();
+            //int length = mHttpURLConnection.getContentLength();
             //Log.d("length", "length=" + length);
 
             if (respondCode == 200) {
