3684f9ad29d301a60d029453f0a12fe9ac817213
huangkaifeng <1195745286@qq.com>
2017-12-04 19:54:55 +0800

    m9流量代码修改。。。。。终于修复了短信校准流量信息

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/TrafficMonitoringActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/TrafficMonitoringActivity.java
@@ -26,7 +26,6 @@ import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitori
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.utils.SystemInfoUtils;
 
 public class TrafficMonitoringActivity extends AppCompatActivity implements View.OnClickListener{
-
     private SharedPreferences mSP;
     private Button mCorrectFlowBtn;
     private TextView mTotalTV;
@@ -37,26 +36,26 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
     private TextView mRemindTV;
     private CorrectFlowReceiver receiver;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traffic_monitoring);
         mSP = getSharedPreferences("config", MODE_PRIVATE);
         boolean flag = mSP.getBoolean("isset_operator", false);
-
-        if (!flag){
+        // 如果没有设置运营商信息则进入信息设置页面
+        if (!flag) {
             startActivity(new Intent(this, OperatorSetActivity.class));
             finish();
         }
         if (!SystemInfoUtils
                 .isServiceRunning(this,
-                        "cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService")){
+                        "cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService")) {
             startService(new Intent(this, TrafficMonitoringService.class));
         }
         initView();
         registReceiver();
-        initDate();
+        initData();
     }
-    private void initView(){
+    private void initView() {
         findViewById(R.id.rl_titlebar).setBackgroundColor(
                 getResources().getColor(R.color.light_green));
         ImageView mLeftImgv = (ImageView) findViewById(R.id.imgv_leftbtn);
@@ -75,15 +74,15 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
         mRemindTV = (TextView) findViewById(R.id.tv_traffic_remind);
     }
 
-    private void initDate(){
-        long totalflow = mSP.getLong("totalflow", 0 );
+    private void initData() {
+        long totalflow = mSP.getLong("totalflow", 0);
         long usedflow = mSP.getLong("usedflow", 0);
-        if (totalflow > 0 & usedflow >= 0){
+        if (totalflow > 0 & usedflow >= 0) {
             float scale = usedflow / totalflow;
-            if (scale > 0.9){
+            if (scale > 0.9) {
                 mRemindIMGV.setEnabled(false);
-                mRemindTV.setText("您的套餐流量即将用完!");
-            }else {
+                mRemindTV.setText("您的套餐流量即将用完！");
+            } else {
                 mRemindIMGV.setEnabled(true);
                 mRemindTV.setText("本月流量充足请放心使用");
             }
@@ -95,21 +94,22 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String dataString = sdf.format(date);
         long moblieGPRS = dao.getMoblieGPRS(dataString);
-        if (moblieGPRS < 0){
+        if (moblieGPRS < 0) {
             moblieGPRS = 0;
         }
         mToDayTV.setText("本日已用：" + Formatter.formatFileSize(this, moblieGPRS));
     }
 
-    private void registReceiver(){
+    private void registReceiver() {
         receiver = new CorrectFlowReceiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction("android.provider.Telephony.SMS_RECEIVED");
         registerReceiver(receiver, filter);
     }
+
     @Override
-    public void onClick(View v){
-        switch (v.getId()){
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.imgv_leftbtn:
                 finish();
                 break;
@@ -117,23 +117,27 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
                 startActivity(new Intent(this,OperatorSetActivity.class));
                 break;
             case R.id.btn_correction_flow:
-
+                // 首先判断是哪个运营商，
                 int i = mSP.getInt("operator", 0);
                 SmsManager smsManager = SmsManager.getDefault();
-                switch (i){
+                switch (i) {
                     case 0:
+                        // 没有设置运营商
                         Toast.makeText(this, "您还没有设置运营商信息", Toast.LENGTH_SHORT).show();
                         break;
                     case 1:
+                        // 中国移动
+                        // 发送cxll至10086
+                        // 获取系统默认的短信管理器
                         smsManager.sendTextMessage("10086", null, "CXLL", null, null);
                         break;
                     case 2:
-                        //联通查流量信息
-                        smsManager.sendTextMessage("10010",null,"CXLL",null,null);
+                        smsManager.sendTextMessage("10001",null,"108",null,null);
+                        // 中国联通
                         break;
                     case 3:
-                        //电信查流量信息
                         smsManager.sendTextMessage("10001",null,"108",null,null);
+                        // 中国电信
                         break;
                 }
         }
@@ -141,35 +145,35 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
 
     class CorrectFlowReceiver extends BroadcastReceiver {
         @Override
-        public void onReceive(Context context, Intent intent){
+        public void onReceive(Context context, Intent intent) {
             Object[] objs = (Object[]) intent.getExtras().get("pdus");
-            for (Object obj : objs){
+            for (Object obj : objs) {
                 SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) obj);
                 String body = smsMessage.getMessageBody();
                 String address = smsMessage.getOriginatingAddress();
-
-                if (!address.equals("10086")){
+                // 以下短信分割只针对中国移动用户
+                if (!address.equals("10086")) {
                     return;
                 }
-                String[] split = body.split(", ");
+                String[] split = body.split("，");
                 System.out.println(split[0]);
-
+                // 本月剩余流量
                 long left = 0;
-
+                // 本月已用流量
                 long used = 0;
-
+                // 本月超出流量
                 long beyond = 0;
-                for (int i=0; i < split.length; i++){
-                    if (split[i].contains("当月常用流量已用")){
-
+                for (int i = 0; i < split.length; i++) {
+                    if (split[i].contains("当月常用流量已用")) {
+                        // 套餐总量
                         String usedflow = split[i].substring(9,
                                 split[i].length());
                         used = getStringTofloat(usedflow);
-                    }else if (split[i].contains("可用")){
+                    } else if (split[i].contains("可用")) {
                         String leftflow = split[i].substring(3,
                                 split[i].length());
                         left = getStringTofloat(leftflow);
-                    }else if (split[i].contains("套餐外流量")){
+                    } else if (split[i].contains("套餐外流量")) {
                         String beyondflow = split[i].substring(6,
                                 split[i].length());
                         beyond = getStringTofloat(beyondflow);
@@ -187,19 +191,19 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
             }
         }
     }
-
-    private long getStringTofloat(String str){
+    /** 将字符串转化成Float类型数据 **/
+    private long getStringTofloat(String str) {
         long flow = 0;
-        if (!TextUtils.isEmpty(str)){
-            if (str.contains("K")){
+        if (!TextUtils.isEmpty(str)) {
+            if (str.contains("K")) {
                 String[] split = str.split("K");
                 float m = Float.parseFloat(split[0]);
                 flow = (long) (m * 1024);
-            }else if (str.contains("M")){
+            } else if (str.contains("M")) {
                 String[] split = str.split("M");
                 float m = Float.parseFloat(split[0]);
                 flow = (long) (m * 1024 * 1024);
-            }else if (str.contains("G")){
+            } else if (str.contains("G")) {
                 String[] split = str.split("G");
                 float m = Float.parseFloat(split[0]);
                 flow = (long) (m * 1024 * 1024 * 1024);
@@ -208,8 +212,8 @@ public class TrafficMonitoringActivity extends AppCompatActivity implements View
         return flow;
     }
     @Override
-    public void onDestroy(){
-        if (receiver != null){
+    public void onDestroy() {
+        if (receiver != null) {
             unregisterReceiver(receiver);
             receiver = null;
         }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/utils/SystemInfoUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/utils/SystemInfoUtils.java
@@ -13,9 +13,9 @@ public class SystemInfoUtils {
     public static boolean isServiceRunning(Context context, String className){
         ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningServiceInfo> infos = am.getRunningServices(200);
-        for (ActivityManager.RunningServiceInfo info:infos){
+        for(ActivityManager.RunningServiceInfo info:infos){
             String serviceClassName = info.service.getClassName();
-            if (className.equals(serviceClassName)){
+            if(className.equals(serviceClassName)){
                 return true;
             }
         }
